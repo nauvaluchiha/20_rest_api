2f8fc08bd60b525760e7ea63af20e505bdab694f
nauvaluchiha <142927902+nauvaluchiha@users.noreply.github.com>
2023-10-05 16:15:51 +0700

    Add files via upload

---
+++ src/stores/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import productData from "./product-slice";
+import productsData from "./products-slice";
+
+export const store = configureStore({
+  reducer: {
+    products: productsData,
+    product: productData,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
